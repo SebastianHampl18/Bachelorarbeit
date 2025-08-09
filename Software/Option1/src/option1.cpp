8a94b13aa62c059e806530ada0843314049da5d7
shampl <sebastian.hampl18@gmail.com>
2025-08-09 20:29:19 +0200

    GPIO Expander Functionen für Write und read vollendet, begonnen Funktionen durch Read/Write I2C zu erweitern, wo sie benötigt werden

--- Software/Option1/src/option1.cpp
+++ Software/Option1/src/option1.cpp
@@ -14,38 +14,55 @@ int SPI_select(int Slave){
   if(Slave == DISPLAY)
   {
     digitalWrite(SPI_CS_DISPLAY_PIN, LOW);
-    return 1;
+    return SUCCESS;
   }
   else if (Slave == Flash)
   {
     digitalWrite(SPI_CS_FLASH_PIN, LOW);
-    return 1;
+    return SUCCESS;
   }
   else if (Slave == CAN2)
   {
     digitalWrite(SPI_CS_CAN2_PIN, LOW);
-    return 1;
+    return SUCCESS;
   }  
   else if (Slave == RFID)
   {
-    // TODO: Set PIN GPIOB 2 on Port Extension to LOW
+    int rv = GPIO_Exp_WriteBit(GPIO_EXP_GPIOB, 2, LOW);
+
+    if(rv == ERROR){
+      perror("ChipSelect RFID failed");
+      return ERROR;
+    }
+
+    return SUCCESS;
   }
   else
   {
     //Error in Defines
-    return -1;
+    return ERROR;
   }
 }
 
-void SPI_deselect(){
+int SPI_deselect(){
 /**
  * @brief This functions Sets every CS to HIGH, no CS is active after this function
+ * 
+ * @return -1: I2C Write failed
+ * @return 1: Bit Successful set
  */
   digitalWrite(SPI_CS_DISPLAY_PIN, HIGH);
   digitalWrite(SPI_CS_FLASH_PIN, HIGH);
   digitalWrite(SPI_CS_CAN2_PIN, HIGH);
 
-  // TODO: Dactivate ChipSelect for RFID
+  int rv = GPIO_Exp_WriteBit(GPIO_EXP_GPIOB, 2, HIGH);
+
+  if(rv == ERROR){
+    perror("ChipSelect RFID could not be Reset");
+    return ERROR;
+  }
+
+  return SUCCESS;
 
 }
 
@@ -57,7 +74,26 @@ int SPI_reset(int Slave){
  * 
  * @return 1 at Success, 0 if there is no Reset for given Slave, -1 at fail
  */
-    // TODO: Reset for given Slave
+  // Reset for given Slave
+  if(Slave == DISPLAY_LCD){
+    digitalWrite(27, HIGH);//TODO: Check for High or LOW active
+
+    //TODO: Wait and Set back to LOW
+    return SUCCESS;
+  }
+  if(Slave == DISPLAY_TOUCH){
+    digitalWrite(32, HIGH); //TODO: Check for High or LOW active
+
+    //TODO: Wait and Set back to LOW
+    return SUCCESS;
+  }
+  if(Slave == RFID){
+    GPIO_Exp_WriteBit(GPIO_EXP_GPIOB, 3, HIGH); //TODO: Check for High or LOW active
+
+    //TODO: Wait and Set back to LOW
+  }
+  
+
 }
 
 int send_RemoteDrive_Request(){
@@ -211,46 +247,217 @@ int recieve_Identification_Request(){
   // TODO: Process Data from RF Module and toggle Status LED
 }
 
+/***************************
+
+I²C Read and Write Functions
+
+****************************/
+
 int GPIO_Exp_WriteRegister(int reg, int value){
+  /**
+ * @brief Writes 8bit Value to a register of the GPIO Expansion
+ * 
+ * @param reg: Register Adress as Hex
+ * @param value: 8bit value to be writen in register
+ * 
+ * @return 1 at Success
+ * @return -1: Writing Address Failed
+ * @return -1: Value out of Range
+ * @return -1: Writing Data Failed
+ * @return -1: Sending Buffer Failed
+ */
+
+  // Check for Value out of Range
+  if(value > 255 || value < 0){
+    // Value out of Range
+    perror("Value out of Range");
+    return ERROR;
+  }
+
+  if(reg<0x00 || (reg < 0x10 && reg > 0x0A) || reg > 0x1A){
+    perror("Register Address out of Range");
+    return ERROR;
+  }
+
+  // Write Address and Write Bit to Buffer
   Wire.beginTransmission(GPIO_EXP_ADRESS);
-  Wire.write(reg);   
-  Wire.write(value);      // Write Data
-  Wire.endTransmission();
+  
+  // Write register Address to Buffer
+  int rv = Wire.write(reg); 
+  if(rv <= 0){
+    perror("Write register Adress failed");
+    return ERROR;
+  }  
+
+  // Write Data to Buffer
+  rv = Wire.write(value);      // Write Data
+  if(rv <= 0){
+    perror("Write data failed");
+    return ERROR;
+  }  
+
+  // Send Buffer to Communication
+  rv = Wire.endTransmission();
+  if(rv != 0){
+    perror("Sending Bufferr to Communication failed");
+    return ERROR;
+  }
+  return SUCCESS;
 }
 
 int GPIO_Exp_ReadRegister(int reg){
-  Wire.beginTransmission(GPIO_EXP_ADRESS);
-  Wire.write(reg);      // Registeradresse senden
-  Wire.endTransmission(false); // Stop-Bedingung vermeiden (Repeated Start)
+  /**
+   * @brief Reads Regsiter from GPIO Expansion
+   * 
+   * @param reg: Register adress in hex
+   * 
+   * @return -1: Write register Address Failed
+   * @return -1: Sendig Data Buffer failed
+   * @return -1: Data Could not be read
+   * @return -1: Register Address out of Range
+   * @return data: Data was read successfully
+   */
 
+  // Check for Address out of Range
+  if(reg<0x00 || (reg < 0x10 && reg > 0x0A) || reg > 0x1A){
+    perror("Register Address out of Range");
+    return ERROR;
+  }
+
+  // Write Address and Write Bit to Buffer
+  Wire.beginTransmission(GPIO_EXP_ADRESS);
+
+  // Write register address to Buffer
+  int rv = Wire.write(reg);              // Registeradresse senden
+  if(rv <= 0){
+    perror("Write Failed");
+    return ERROR;
+  }
+
+  // Send Buffer to Communication
+  rv = Wire.endTransmission(false);  // Stop-Bedingung vermeiden (Repeated Start)
+  if(rv != 0){
+    perror("sending Data failed");
+    return ERROR;
+  }
+
+  // Write Address and Read bit to Buffer
   Wire.requestFrom(GPIO_EXP_ADRESS, (uint8_t)1);
+
+  // Read Register from Communication
   if (Wire.available()) {
     return Wire.read(); // Byte zurückgeben
   }
   return ERROR; // Fehlerwert
 }
 
-
 int GPIO_Exp_WriteBit(int reg, int bit, int value){
 
+  /**
+   * @brief Reads the Value from addressed Register and only changes designated bit without touching rest of the register
+   * 
+  * @param reg: Regsiteradress in hex
+  * @param bit: Bitposition to be changed at
+  * @param value: Binary Value to be set at bit position
+  *
+  * @return -1: Register Read Error
+  * @return -1: Non Binary Value
+  * @return -1: Register Write Error
+  * @return -1: Data Send Error
+  * @return -1: Register Address out of Range
+  * @return 1: Writing Successful
+  */
+
+  // Check for value in Range
+  if(value > 1 || value < 0){
+    perror("Value must be binary");
+    return ERROR;
+  }
+
+  // Check for Address out of Range
+  if(reg<0x00 || (reg < 0x10 && reg > 0x0A) || reg > 0x1A){
+    perror("Register Address out of Range");
+    return ERROR;
+  }
+
+  // Set Address and Write Bit
   Wire.beginTransmission(GPIO_EXP_ADRESS);
-  Wire.write(reg);   
+
+  // Write Register Adress to Buffer
+  int rv = Wire.write(reg);  
+  if(rv <= 0){
+    perror("Writing Failed");
+    return ERROR;
+  } 
+
+  // Read Data from Register
   int reg_value = GPIO_Exp_ReadRegister(reg);
   if(reg_value == ERROR){
-    Wire.endTransmission();
+    perror("Register could not be read");
     return ERROR;
   }
+
+  // Changes Bit in Register Data and write data to Buffer
   if(value == HIGH){
-    Wire.write(reg_value | (value << bit)); 
-    Wire.endTransmission();
-    return SUCCESS;
+    rv = Wire.write(reg_value | (value << bit)); 
+    if(rv <= 0){
+      perror("Writing Failed");
+      return ERROR;
+    }
   }
   else if(value == LOW){
-    Wire.write(reg_value & (value << bit));
-    Wire.endTransmission();
-    return SUCCESS;
+    rv = Wire.write(reg_value & (value << bit));
+    if(rv <= 0){
+      perror("Writing Failed");
+      return ERROR;
+    }
   }
-  else
-    Wire.endTransmission();
+  
+  // Send Buffer to Communication
+  rv = Wire.endTransmission();
+  if(rv != 0){
+    perror("Error sending data via I2C");
     return ERROR;
+  }
+  return SUCCESS;  
+}
+
+int GPIO_Exp_ReadBit(int reg, int bit){
+
+  /**
+   * @brief Reads one single Bit from registers of the GPIO Expansion
+   * 
+   * @param reg: 8bit Register Address in Hex
+   * @param bit: Bit position to be read
+   * 
+   * @return -1: Bit Value ot of Range
+   * @return -1: Register Address out of range
+   * @return -1: Read register failed
+   * @return bit_value: Data read and processed successfully 
+   */
+
+   // Check for Bit Value out of Range
+  if(bit > 7){
+    perror("Value out of Range");
+    return ERROR;
+  }
+
+  // Check for Address out of Range
+  if(reg<0x00 || (reg < 0x10 && reg > 0x0A) || reg > 0x1A){
+    perror("Register Address out of Range");
+    return ERROR;
+  }
+
+  // Read Register from GPIO Port Expansion
+  int value = GPIO_Exp_ReadRegister(reg);
+  if(value == ERROR){
+    perror("Read Register Failed");
+    return ERROR;
+  }
+
+  // mask and filter for single bit
+  u_int8_t bit_value = value >> bit;
+  bit_value &= 0x01;
+
+  return bit_value;
 }
